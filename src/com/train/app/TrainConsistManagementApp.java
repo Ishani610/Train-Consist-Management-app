e1f85e8eb125efa18402b01373a3d3e57eabe69c
Ishani610 <ishanisaxena04@gmail.com>
2026-03-25 13:51:00 +0530

    Base application setup

---
+++ src/com/train/app/TrainConsistManagementApp.java
@@ -0,0 +1,7 @@
+package com.train.app;
+
+public class TrainConsistManagementApp {
+    public static void main(String[] args) {
+        System.out.println("Train Consist Management App Started");
+    }
+}
